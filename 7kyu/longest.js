1ad84a39cf99f9d07cb8421017c8c26fb7895232
iparkgood <zuzu8723@gmail.com>
2021-04-27 22:32:32 -0400

    refactored longest

--- 7kyu/longest.js
+++ 7kyu/longest.js
@@ -1,14 +1,5 @@
 function longest(s1, s2) {
-  let longestArr = [];
-  const twoArrs = (s1 + s2).split("");
-
-  for (let i = 0; i < twoArrs.length; i++) {
-    if (twoArrs[i] !== twoArrs[i + 1] && !longestArr.includes(twoArrs[i])) {
-      longestArr.push(twoArrs[i]);
-    }
-  }
-
-  return longestArr.sort().join("");
+  return [...new Set(s1 + s2)].sort().join("");
 }
 
 console.log(longest("aretheyhere", "yestheyarehere")); //"aehrsty"
