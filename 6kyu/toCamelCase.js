b4d6eabd22e1c554142fd6406e39686dfb6ccdd2
iparkgood <zuzu8723@gmail.com>
2021-05-02 17:55:26 -0400

    solved toCamelCase

---
+++ 6kyu/toCamelCase.js
@@ -0,0 +1,20 @@
+function toCamelCase(str) {
+  if (str === "") return "";
+
+  const words = str.replace(/(-|_)/g, " ").split(" ");
+
+  return words
+    .map((word, idx) =>
+      idx === 0 ? word : word[0].toUpperCase() + word.slice(1)
+    )
+    .join("");
+}
+
+console.log(toCamelCase(""));
+// '', "An empty string was provided but not returned"
+console.log(toCamelCase("the_stealth_warrior"));
+// "theStealthWarrior", "toCamelCase('the_stealth_warrior') did not return correct value"
+console.log(toCamelCase("The-Stealth-Warrior"));
+// "TheStealthWarrior", "toCamelCase('The-Stealth-Warrior') did not return correct value"
+console.log(toCamelCase("A-B-C"));
+// "ABC", "toCamelCase('A-B-C') did not return correct value"
