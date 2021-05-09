a76eedef59dc1407778c6f0d9154bdaba81dceee
iparkgood <zuzu8723@gmail.com>
2021-05-09 18:53:31 -0400

    fixed moveZeros

--- 5kyu/moveZeros.js
+++ 5kyu/moveZeros.js
@@ -1,5 +1,5 @@
 var moveZeros = function (arr) {
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = arr.length - 1; i >= 0; i--) {
     if (arr[i] === 0) {
       arr.splice(i, 1);
       arr.push(0);
@@ -10,3 +10,6 @@ var moveZeros = function (arr) {
 };
 
 console.log(moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1])); //[ 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 ]
+console.log(
+  moveZeros([9, 0, 9, 1, 2, 1, 1, 3, 1, 9, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0])
+); //[9,9,1,2,1,1,3,1,9,9,0,0,0,0,0,0,0,0,0,0]
