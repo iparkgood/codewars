c3f45407d5b2f412e9be264057203a494a8432ea
iparkgood <zuzu8723@gmail.com>
2021-05-09 20:26:05 -0400

    solved list

---
+++ 6kyu/list.js
@@ -0,0 +1,26 @@
+function list(names) {
+  const nameArr = names.map((name) => name.name);
+
+  if (nameArr.length === 0 || names === []) return "";
+  if (nameArr.length === 1) return nameArr.join("");
+
+  return (
+    nameArr.slice(0, nameArr.length - 1).join(", ") +
+    " & " +
+    nameArr[nameArr.length - 1]
+  );
+}
+
+console.log(
+  list([
+    { name: "Bart" },
+    { name: "Lisa" },
+    { name: "Maggie" },
+    { name: "Homer" },
+    { name: "Marge" },
+  ])
+); //'Bart, Lisa, Maggie, Homer & Marge'
+console.log(list([{ name: "Bart" }, { name: "Lisa" }, { name: "Maggie" }])); //'Bart, Lisa & Maggie'
+console.log(list([{ name: "Bart" }, { name: "Lisa" }])); //'Bart & Lisa'
+console.log(list([{ name: "Bart" }])); //'Bart'
+console.log(list([])); //''
