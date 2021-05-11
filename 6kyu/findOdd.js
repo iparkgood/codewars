d9f1c9af42dd04fd36a8152a62d4b8c4e3c379fd
iparkgood <zuzu8723@gmail.com>
2021-05-11 17:43:22 -0400

    solved findOdd.js

---
+++ 6kyu/findOdd.js
@@ -0,0 +1,24 @@
+function findOdd(A) {
+  const Nums = {};
+
+  for (let i = 0; i < A.length; i++) {
+    if (Nums.hasOwnProperty(A[i])) {
+      Nums[A[i]]++;
+    } else {
+      Nums[A[i]] = 1;
+    }
+  }
+
+  for (const prop in Nums) {
+    if (Nums[prop] % 2 !== 0) {
+      return +prop;
+    }
+  }
+}
+
+console.log(findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5])); // 5
+console.log(findOdd([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5])); // -1
+console.log(findOdd([20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5])); // 5
+console.log(findOdd([10])); // 10
+console.log(findOdd([1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1])); // 10
+console.log(findOdd([5, 4, 3, 2, 1, 5, 4, 3, 2, 10, 10])); // 1
