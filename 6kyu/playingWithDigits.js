9659817fb347517d09175ecfd5b9d30c85c3604b
iparkgood <zuzu8723@gmail.com>
2021-05-13 15:54:58 -0400

    solved digPow

---
+++ 6kyu/playingWithDigits.js
@@ -0,0 +1,15 @@
+function digPow(n, p) {
+  let sum = 0;
+  const nums = [...n.toString()].map((num) => +num);
+
+  for (let i = 0; i < nums.length; i++) {
+    sum += Math.pow(nums[i], p);
+    p++;
+  }
+
+  return Number.isInteger(sum / n) ? sum / n : -1;
+}
+
+console.log(digPow(89, 1)); // 1
+console.log(digPow(92, 1)); // -1
+console.log(digPow(46288, 3)); // 51
