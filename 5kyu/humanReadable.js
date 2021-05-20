b0947dc954022d275f02b0bf144d591c2336dff1
iparkgood <zuzu8723@gmail.com>
2021-05-20 14:21:25 -0400

    removed comments

--- 5kyu/humanReadable.js
+++ 5kyu/humanReadable.js
@@ -7,15 +7,9 @@ function humanReadable(seconds) {
 
   return `${hours}:${minutes}:${remainingSec}`;
 }
+
 console.log(humanReadable(0)); //'00:00:00'
 console.log(humanReadable(5)); //'00:00:05'
 console.log(humanReadable(60)); //'00:01:00'
 console.log(humanReadable(86399)); //'23:59:59'
 console.log(humanReadable(359999)); //'99:59:59'
-
-/*
-HH = hours, padded to 2 digits, range: 00 - 99
-MM = minutes, padded to 2 digits, range: 00 - 59
-SS = seconds, padded to 2 digits, range: 00 - 59
-The maximum time never exceeds 359999 (99:59:59)
-*/
