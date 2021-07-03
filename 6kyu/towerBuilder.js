d91d1954d8ee7e0616c037749c1b5d5aaa69426a
iparkgood <zuzu8723@gmail.com>
2021-07-02 21:50:38 -0400

    solved towerBuilder

---
+++ 6kyu/towerBuilder.js
@@ -0,0 +1,17 @@
+function towerBuilder(nFloors) {
+  let tower = [];
+
+  for (let i = 0; i < nFloors; i++) {
+    tower.push(
+      " ".repeat(nFloors - i - 1) +
+        "*".repeat(2 * i + 1) +
+        " ".repeat(nFloors - i - 1)
+    );
+  }
+
+  return tower;
+}
+
+console.log(towerBuilder(1)); //["*"]
+console.log(towerBuilder(2)); //[" * ","***"]
+console.log(towerBuilder(3)); //["  *  "," *** ","*****"]
